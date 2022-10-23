36e2f92b4dd9a220e352275c071ed88efb750683
Simon H <github@pointbeing.net>
2022-10-23 16:19:07 +0100

    Tags, full import etc. Still has bugs

--- import_scripts/content.py
+++ import_scripts/content.py
@@ -1,14 +1,60 @@
-from pprint import pprint
+from dateutil.parser import parse
+# from pprint import pprint
 import re
 import urllib.request
 
 from grab import Grab
 
-
+# Run-specific config
 # FETCH_IMAGES = True
 FETCH_IMAGES = False
+LIMIT = 300
 
-LIMIT = 3 # 00
+
+# SQL templates
+SQL_MAIN = """INSERT INTO 3cbposts VALUES(
+    NULL,
+    1,
+    '{post_date}',
+    '{post_date}',
+    '{post_content}',
+    '{post_title}',
+    '',
+    'publish',
+    'open',
+    'open',
+    '',
+    '{post_name}',
+    '',
+    '',
+    '{post_date}',
+    '{post_date}',
+    '',
+    0,
+    '{post_name}',
+    0,
+    'post',
+    '',
+    0
+);\n"""
+
+# SQL_SETVAR = 'SET @liid=last_insert_id()\n'
+
+SQL_ADD_TAG = """INSERT INTO 3cbterm_relationships VALUES (
+    last_insert_id(),
+    (SELECT term_taxonomy_id FROM 3cbterm_taxonomy WHERE term_id =
+         (SELECT term_id FROM 3cbterms WHERE name='{tag_name}')
+     ),
+    0
+); \n"""
+
+SQL_ADD_CAT = """INSERT INTO 3cbterm_relationships VALUES (
+    last_insert_id(),
+    7,
+    0
+); \n"""
+
+
 DL_DIR = './_dl/'
 
 BASE_URL = 'https://threehundredbeers.com'
@@ -27,14 +73,21 @@ for url in beer_urls:
 
     beer_page = BASE_URL + url.attr('href')
 
-    print('Fetching: ', beer_page)
+    #print('Fetching: ', beer_page)
 
     g.go(beer_page)
 
     post = {}
 
+    # slug
+    post['post_name'] = beer_page.split('/').pop()
+
     # title
     post['title'] = g.doc.select('//title')[0].text()
+
+    # reformat publishing date
+    pub_date = g.doc.select('//div[@class="date"]')[0].text()
+    post['post_date'] = parse(pub_date).strftime('%Y-%m-%d')
 
     # tags
     tags = g.doc.select('//div[@class="tags"]/p/a')
@@ -64,13 +117,36 @@ for url in beer_urls:
         if FETCH_IMAGES:
             urllib.request.urlretrieve(url, DL_DIR + filename)
 
+        ### POSSIBLY A BUG HERE
         regexp = re.compile(r'https://(.*)' + filename, re.DOTALL)
         body = regexp.sub(HOST_FOLDER + filename, body)
 
+    # body should be built by now
     post['body'] = body
+    # post['body'] = 'hello again, world'
 
-    pprint(post)
 
+    # pprint(post)
+
+    sql = SQL_MAIN.format(
+            post_date = post['post_date'],
+            post_content = post['body'].replace('"','\\"').replace("'","\\'")   ,
+            post_title = post['title'].replace("'","\\'"),
+            post_name = post['post_name']
+        )
+
+    # sql = sql + SQL_SETVAR
+
+    # add tags
+    for tag in post['tags']:
+        sql = sql+ SQL_ADD_TAG.format(tag_name=tag)
+
+    sql = sql + SQL_ADD_CAT
+
+    print(sql)
+
+
+    # break if necessary
     ctr += 1
 
     if ctr >= LIMIT:
